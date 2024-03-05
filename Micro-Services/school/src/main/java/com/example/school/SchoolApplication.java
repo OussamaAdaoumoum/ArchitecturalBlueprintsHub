86adafcbd347211047a8d9beb4a9d009a354e2d0
oussamaAdaoumoum <oussamaadm755@gmail.com>
2024-03-05 16:31:47 +0100

    distribute the configuration in the microservices architecture

--- Micro-Services/school/src/main/java/com/example/school/SchoolApplication.java
+++ Micro-Services/school/src/main/java/com/example/school/SchoolApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @SpringBootApplication
 public class SchoolApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolApplication.class, args);
 	}
-
 }
